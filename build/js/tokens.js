3c2cf048f4634e8f672c7d6b9d3813560c46bfde
DarcioCarvalho <darcio.nuno@gmail.com>
2023-11-26 15:08:59 -0300

    :sparkles: feat: initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,19 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 26 Nov 2023 18:07:41 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1e",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6",
+  "VariableCollectionGray200": "#c1c1c4ff",
+  "VariableCollectionGray500": "#50505cff",
+  "VariableCollectionGray800": "#1c1c1eff",
+  "VariableCollectionGray900": "#121214ff",
+  "VariableCollectionWhite": "#ffffffff",
+  "VariableCollectionPurple500": "#8257e6ff"
+};
